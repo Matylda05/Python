f5f9ca3d2944034b558d173d67f6bf09162040a1
Matylda <matyldataborek20@gmail.com>
2026-01-03 13:10:26 +0100

    c

--- Projekt/Projekt.py
+++ Projekt/Projekt.py
@@ -6,15 +6,28 @@ root.geometry("900x500")
 root.configure(bg="#d4f1f9")
 
 
+value_const = tk.StringVar(value="")
 value = tk.StringVar(value="")
-text = tk.Label(root, textvariable=value,
-                font=("Times New Roman", 30), 
-                bg="#d4f1f9")
+
+text = tk.Label(
+    root,
+    font=("Times New Roman", 30),
+    bg="#d4f1f9"
+)
 text.pack(pady=20)
+
+def refresh_label():
+    text.config(text=value_const.get() + value.get())
 
 frame = tk.Frame(root)
 frame.pack(expand=True)
 
+B_WIDTH = 6
+B_HEIGHT = 1
+
+def wpisz(x):
+    value.set(value.get() + x)
+    refresh_label()
 
 button1 = tk.Button(
                     frame,
@@ -24,88 +37,119 @@ button1 = tk.Button(
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "1"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("1")
+                    )
 
 button2 = tk.Button(
                     frame,
-                    text="2",
+                    text="2 abc",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "2"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("2")
+                    )
 
 button3 = tk.Button(
                     frame,
-                    text="3",
+                    text="3 def",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "3"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("3")
+                    )
 
 button4 = tk.Button(
                     frame,
-                    text="4",
+                    text="4 ghi",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "4"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("4")
+                    )
 
 button5 = tk.Button(
                     frame,
-                    text="5",
+                    text="5 jkl",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "5"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("5")
+                    )
 
 button6 = tk.Button(
                     frame,
-                    text="6",
+                    text="6 mno",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "6"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("6")
+                    )
 
 button7 = tk.Button(
                     frame,
-                    text="7",
+                    text="7 pqrs",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "8"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("7")
+                    )
 
 button8 = tk.Button(
                     frame,
-                    text="8",
+                    text="8 tuv",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "8"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("8")
+                    )
 
 button9 = tk.Button(
                     frame,
-                    text="9",
+                    text="9 wxyz",
                     font=("Times New Roman", 30, "bold"),
                     bg="#9ae2f5",
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "9"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("9")
+                    )
 
+def spacja():
+    value_const.set(value_const.get() + value.get() + " ")
+    value.set("")
+    refresh_label()
 
 button_s = tk.Button(
                     frame,
@@ -115,7 +159,9 @@ button_s = tk.Button(
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + " "))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=spacja)
 
 button0 = tk.Button(
                     frame,
@@ -125,7 +171,19 @@ button0 = tk.Button(
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get() + "0"))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: wpisz("0")
+                    )
+
+def backspace():
+    if value.get() == "":
+        value_const.set(value_const.get()[:-1])
+        refresh_label()
+    else:
+        value.set(value.get()[:-1])
+        refresh_label()
+
 
 button_b = tk.Button(
                     frame,
@@ -135,7 +193,10 @@ button_b = tk.Button(
                     fg="black",
                     activebackground="#0590b5",
                     activeforeground="black",
-                    command=lambda: value.set(value.get()))
+                    width=B_WIDTH,
+                    height=B_HEIGHT,
+                    command=lambda: backspace()
+                    )
 
 
 button1.grid(row=1, column=0, padx=5, pady=5)
